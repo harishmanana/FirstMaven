92303dd59a0db5a60a8387a733ee8ba37baa537a
Harish Manana <harishmanana@gmail.com>
2018-01-08 10:35:53 +0530

    Release 2

--- src/main/java/com/java/first/HelloApp.java
+++ src/main/java/com/java/first/HelloApp.java
@@ -6,6 +6,9 @@ public class HelloApp {
 		System.out.println("Hello World");
 		
 		System.out.println("New line added");
+		
+		TestGit obj = new TestGit();
+		System.out.println(obj.getMsg());
 	}
 
 }
---
+++ src/main/java/com/java/first/TestGit.java
@@ -0,0 +1,7 @@
+package com.java.first;
+
+public class TestGit {
+	public String getMsg() {
+		return "Hello How are you !!!";
+	}
+}
