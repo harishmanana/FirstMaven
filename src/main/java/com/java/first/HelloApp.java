305fdb9bbb7e10f25e975cfbe15b2e84f2c5b6b6
Harish Manana <harishmanana@gmail.com>
2018-01-08 10:31:02 +0530

    Release 1

---
+++ src/main/java/com/java/first/HelloApp.java
@@ -0,0 +1,11 @@
+package com.java.first;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("New line added");
+	}
+
+}
